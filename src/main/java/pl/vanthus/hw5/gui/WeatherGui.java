ed8b34cd0a3798256756389a70efd22509b045e8
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-10-20 15:39:57 +0200

    Added fields to input data

--- src/main/java/pl/vanthus/hw5/gui/WeatherGui.java
+++ src/main/java/pl/vanthus/hw5/gui/WeatherGui.java
@@ -1,11 +1,33 @@
 package pl.vanthus.hw5.gui;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("weather")
 public class WeatherGui extends VerticalLayout {
 
+    TextField cityField;
+    ListBox<String> unitsListBox;
+    Button getWeatherButton;
+
+    public WeatherGui() {
+
+        cityField = new TextField("City");
+
+        Label unitsLabel = new Label("Choose unit");
+        unitsListBox = new ListBox<>();
+        unitsListBox.setItems("Fahrenheit", "Celsius", "Kelvin");
+
+        getWeatherButton = new Button("Check Weather", new Icon(VaadinIcon.SUN_RISE));
 
 
+        add(cityField, unitsLabel, unitsListBox, getWeatherButton);
+
+    }
 }
